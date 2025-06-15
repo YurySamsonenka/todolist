ef18a03ea41fb05b96fa289bd676deac57a89fe7
YurySamsonenka <cant09y@gmail.com>
2025-06-15 21:18:26 +0300

    feat: refactor todolists reducer by removing commented-out code

--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -3,34 +3,6 @@ import { createAction, createReducer, nanoid } from '@reduxjs/toolkit';
 
 const initialState: Todolist[] = [];
 
-/*export const todolistsReducer = (state: Todolist[] = initialState, action: Actions): Todolist[] => {
-	switch (action.type) {
-		case 'delete_todolist': {
-			return state.filter(todolist => todolist.id !== action.payload.id);
-		}
-		case 'create_todolist': {
-			const newTodolist: Todolist = {
-				id: action.payload.id,
-				title: action.payload.title,
-				filter: 'all',
-			};
-			return [newTodolist, ...state];
-		}
-		case 'change_todolist_title' : {
-			return state.map(todolist => todolist.id === action.payload.id
-				? { ...todolist, title: action.payload.title }
-				: todolist);
-		}
-		case 'change_todolist_filter' : {
-			return state.map(todolist => todolist.id === action.payload.id
-				? { ...todolist, filter: action.payload.filter }
-				: todolist);
-		}
-		default:
-			return state;
-	}
-};*/
-
 export const createTodolistAC = createAction('todolists/createTodolist', (title: string) => {
 	return { payload: { title, id: nanoid() } };
 });
@@ -67,8 +39,3 @@ export const todolistsReducer = createReducer(initialState, (builder) => {
 			}
 		})
 });
-
-
-
-export type CreateTodolistAction = ReturnType<typeof createTodolistAC>
-export type DeleteTodolistAction = ReturnType<typeof deleteTodolistAC>
